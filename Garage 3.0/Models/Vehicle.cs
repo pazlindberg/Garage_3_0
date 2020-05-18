52638a3b0c2642ae1285a50805e9b5279367949f
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-05-18 13:56:54 +0200

    klasserna börjar byggas

---
+++ Garage 3.0/Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_3._0.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int NrOfWheels { get; set; }
+
+        public int MemberId { get; set; }
+        public int VehicleTypeId { get; set; }
+        
+        public Member Member { get; set; }
+        public VehicleType VehicleType { get; set; }
+    }
+}
---
+++ Garage 3.0/Models/VehicleType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_3._0.Models
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public double size { get; set; } //hur stor del av en parkeringsplats tar denna fordonstyp?
+    }
+}
