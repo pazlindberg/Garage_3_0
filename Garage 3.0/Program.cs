7058094b1830c44046392691638b11362e82dbd0
paz.lindberg@gmail.com <paz.lindberg@gmail.com>
2020-05-19 10:17:06 +0200

    0519init

--- Garage 3.0/Program.cs
+++ Garage 3.0/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+//...
 namespace Garage_3._0
 {
     public class Program
